a369cc108dc74ce252cebb7d8c24f0898c490257
JamesleeG <jamesleegambardella@gmail.com>
2021-03-10 15:01:06 -0500

    Changes for deployment

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://enigmatic-sands-17088.herokuapp.com/',
+  production: 'https://enigmatic-sands-17088.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
